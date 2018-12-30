80cbc6775e269c1f510d07b5f7935c8e264f595d
shahriar <shahriar_cse@live.com>
2018-12-30 13:58:22 +0600

    redux implemented

--- App.js
+++ App.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import CounterApp from './src/CounterApp';
+import { createStore } from 'redux';
+import {Provider} from 'react-redux';
+
+const initialState = {
+  counter: 3
+}
+const reducer = (state = initialState, action)=>{
+  switch(action.type){
+    case 'INCREASE_COUNTER' : return { counter : state.counter + 1}
+    case 'DECREASE_COUNTER' : return { counter : state.counter - 1}
+  }
+  return state;
+}
+const store = createStore(reducer);
 
 export default class App extends React.Component {
   render() {
     return (
-      <CounterApp/>
+      <Provider store={store} >
+        <CounterApp/>
+      </Provider>
     );
   }
 }
--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,26 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import {connect} from 'react-redux';
 
-export default class CounterApp extends React.Component {
-    state = {
-        counter: 0
-    }
-    increaseCounter = ()=>{
-        this.setState({counter: this.state.counter + 1})
-    }  
-    decreaseCounter = ()=>{
-        this.setState({counter: this.state.counter - 1})
-    }
-
+class CounterApp extends React.Component {
     render() {
         return (
         <View style={styles.container}>
             <View style={{flexDirection:'row', justifyContent:'space-around', width:300}} >
-            <TouchableOpacity onPress={()=>this.increaseCounter() } >
+            <TouchableOpacity onPress={()=>this.props.increaseCounter() } >
                 <Text style={{fontSize:20}}>Increase</Text>
             </TouchableOpacity>
-            <Text style={{fontSize:20}}>{this.state.counter}</Text>
-            <TouchableOpacity onPress={()=>this.decreaseCounter()} >
+            <Text style={{fontSize:20}}>{this.props.counter}</Text>
+            <TouchableOpacity onPress={()=>this.props.decreaseCounter()} >
                 <Text style={{fontSize:20}}>Decrease!</Text>
             </TouchableOpacity>          
             </View>
@@ -29,6 +20,20 @@ export default class CounterApp extends React.Component {
     }
 }
 
+function mapStateToProps(state){
+    return {
+        counter : state.counter
+    }
+}
+
+function mapDispatchToProps(dispatch){
+    return {
+        increaseCounter: ()=> {dispatch({type: 'INCREASE_COUNTER'})},
+        decreaseCounter: ()=> {dispatch({type: 'DECREASE_COUNTER'})}
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterApp);
 
 const styles = StyleSheet.create({
   container: {
