31cef6a91ea5ce55c15d33d57109cacef7bdac68
shahriar <shahriar_cse@live.com>
2018-12-31 10:57:41 +0600

    replace custom logger with redux-logger
    added redux-logger from npm, replacing our custom logger coz the redux-logger offer more info in log, plus it comes with highlight color.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,7 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers';
-import customLogger from '../middleware/customMiddleware';
+//import customLogger from '../middleware/customMiddleware';
+import customLogger from 'redux-logger';
 
 export default store = createStore(rootReducer, applyMiddleware(customLogger));
 
